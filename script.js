03d0952afb0ed8c8bda55534ccae81db1673ea14
Rajdeep Singh <rajdeepsingh.lmb@gmail.com>
2024-11-13 00:39:13 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// JavaScript to toggle visibility of images
+function toggleImageVisibility(sectionId) {
+    const section = document.getElementById(sectionId);
+    if (section.style.display === 'none') {
+        section.style.display = 'block';
+    } else {
+        section.style.display = 'none';
+    }
+}
